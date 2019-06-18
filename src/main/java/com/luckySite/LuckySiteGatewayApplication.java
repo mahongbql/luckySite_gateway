b064f383943cc6e5ca1b2106d38e902551394d16
mahongbin <mahongbin@youxin.com>
2019-06-18 17:17:25 +0800

    第一次提交

---
+++ src/main/java/com/luckySite/LuckySiteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.luckySite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckySiteGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckySiteGatewayApplication.class, args);
+    }
+
+}
